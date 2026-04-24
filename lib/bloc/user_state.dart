a41f196fdded758f48879014441e5e632d5fa374
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-04-24 12:46:07 +0700

    add class UserState dengan initial, loading, loaded, and error states  untuk manajemen user

---
+++ lib/bloc/user_state.dart
@@ -0,0 +1,14 @@
+
+import 'package:sqlite/domain/entities/user_entity.dart';
+
+abstract class UserState {}
+  class UserInitial extends UserState {}
+  class UserLoading extends UserState {}
+  class UserLoaded extends UserState {
+    final List<UserEntity> users;
+    UserLoaded(this.users);
+  }
+  class UserError extends UserState {
+    final String message;
+    UserError(this.message);
+  }
