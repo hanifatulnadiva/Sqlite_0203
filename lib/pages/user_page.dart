a5fc8e3990bbaf6861c6dc52f69a8a03cf15c32f
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-04-24 13:08:57 +0700

    menambahkan sized box untuk buton simpan dan menghubungkan  dengan bloc untuk edit dan tambah

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sqlite/bloc/user_bloc.dart';
+import 'package:sqlite/bloc/user_event.dart';
 import 'package:sqlite/domain/entities/user_entity.dart';
 
 class UserFormPage extends StatefulWidget {
@@ -14,42 +17,49 @@ class _UserFormPageState extends State<UserFormPage> {
   final _emailController = TextEditingController();
 
   @override
-  void initState() {
+  void initState(){
     super.initState();
-    if (widget.user != null) {
+    if(widget.user != null){
       _nameController.text = widget.user!.name;
       _emailController.text = widget.user!.email;
     }
   }
-
   @override
   Widget build(BuildContext context) {
     final isEditing = widget.user != null;
     return Scaffold(
-      appBar: AppBar(title: Text(isEditing ? "Edit User" : "Tambah User")),
+      appBar: AppBar(title:Text(isEditing ? "Edit User":"Tambah User")),
       body: Padding(
-        padding: EdgeInsetsGeometry.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _nameController,
-              decoration: InputDecoration(
-                labelText: "Nama Lengkap",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: "Email",
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 16.0),
-          ],
-        ),
-      ),
+        padding:EdgeInsetsGeometry.all(16.0),
+        child:Column(children: [
+          TextField(controller:_nameController,
+          decoration:InputDecoration(labelText:"Nama Lengkap", border:OutlineInputBorder()),
+          ),
+          SizedBox(height:16.0),
+          TextField(controller:_emailController,
+          decoration:InputDecoration(labelText:"Email", border:OutlineInputBorder()),
+          ),
+          const SizedBox(height:16.0),
+          SizedBox(
+            width: double.infinity,
+            height: 58,
+            child:ElevatedButton(onPressed: (){
+              final newUser=UserEntity(
+                id: isEditing ?widget.user!.id :DateTime.now().millisecondsSinceEpoch.toString(),
+                name: _nameController.text,
+                email: _emailController.text,
+              );
+              if(isEditing){
+                context.read<UserBloc>().add(UpdateUserEvent(newUser));
+              }else
+              {
+                context.read<UserBloc>().add(AddUserEvent(newUser));
+              }
+              Navigator.pop(context);
+            }, child: Text(isEditing ? "SimpanPerubahan" : "Simpan User Baru"))
+          )
+        ],
+      ),)
     );
   }
 }
