7b41cc0abb3013df5c95b647522b5b82f99ba583
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-04-24 16:29:32 +0700

    menambahkan variabel nohp dan alamat beserta  konstruktor di class UserEntity

--- lib/domain/entities/user_entity.dart
+++ lib/domain/entities/user_entity.dart
@@ -2,5 +2,8 @@ class UserEntity {
   final String id;
   final String name;
   final String email;
-  UserEntity({required this.id, required this.name, required this.email});
+  final String nohp;
+  final String alamat;
+
+  UserEntity({required this.id, required this.name, required this.email,required this.nohp,  required this.alamat});
 }
