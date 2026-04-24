5bb7870e7d67139217d348d25cac0835c9b2b58c
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2026-04-24 23:17:21 +0700

    menambahkan style untuk halaman home

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,7 +11,15 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title:const Text('Daftar User')),
+      backgroundColor: const Color(0xFFB8AFAF),
+      appBar: AppBar(
+        title:const Text('Daftar User',
+          style: TextStyle(fontSize: 30,
+          fontWeight: FontWeight.bold,
+          color: Colors.white)),
+        centerTitle: true,
+        backgroundColor: const Color(0xFFB8AFAF),
+      ),
       body:BlocBuilder<UserBloc, UserState>(
         builder: (context,state){
           if(state is UserLoading) return const Center(child: CircularProgressIndicator());
@@ -20,24 +28,85 @@ class HomePage extends StatelessWidget {
               itemCount:state.users.length,
               itemBuilder: (context, index){
                 final user =state.users[index];
-                return ListTile(
-                  title: Text(user.name),
-                  subtitle: Text(user.email),
-                  trailing:Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.edit, color:Colors.blue),
-                        onPressed: () =>Navigator.push(context,
-                        MaterialPageRoute(builder:(_) => UserFormPage(user:user)),
+                return Card(
+                  margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+                  elevation: 3,
+                  color: const Color(0xFFF2EAE0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            const Icon(Icons.person, size: 18),
+                            const SizedBox(width: 6),
+                            Text(
+                              user.name,
+                              style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.delete, color:Colors.red),
-                        onPressed: () => context.read<UserBloc>().add(DeleteUserEvent(user.id)),
-                      ),
-                    ],
-                  )
+                        const SizedBox(height: 6),
+                        Row(
+                          children: [
+                            const Icon(Icons.email, size: 16),
+                            const SizedBox(width: 6),
+                            Text(user.email),
+                          ],
+                        ),
+
+                        const SizedBox(height: 4),
+                        Row(
+                          children: [
+                            const Icon(Icons.phone, size: 16),
+                            const SizedBox(width: 6),
+                            Text(user.nohp ?? "-"),
+                          ],
+                        ),
+
+                        const SizedBox(height: 4),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Icon(Icons.location_on, size: 16),
+                            const SizedBox(width: 6),
+                            Expanded(
+                              child: Text(user.alamat ?? "-"),
+                            ),
+                          ],
+                        ),
+
+                        const SizedBox(height: 8),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.edit, color: Colors.blue),
+                              onPressed: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => UserFormPage(user: user),
+                                ),
+                              ),
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.delete, color: Colors.red),
+                              onPressed: () => context
+                                  .read<UserBloc>()
+                                  .add(DeleteUserEvent(user.id)),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                 );
               },
             );
@@ -46,11 +115,12 @@ class HomePage extends StatelessWidget {
         },
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.brown,
         onPressed: () => Navigator.push(
           context,
           MaterialPageRoute(builder: (_) => const UserFormPage()),
         ),
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.add, color: Colors.white,),
       ),
       
     );
